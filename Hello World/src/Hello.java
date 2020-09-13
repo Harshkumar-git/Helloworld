07599eb27fdcc9ccef0f22c7e677c41efd1f66de
harshkumarpatel <harshkumarpatel@localhost>
2020-09-13 12:46:53 -0400

    First commit

---
+++ Hello World/src/Hello.java
@@ -0,0 +1,12 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World ");
+		int value1 = 1;
+		int value2 = (2*2)-1;
+		System.out.println(value2);
+	}
+
+}
